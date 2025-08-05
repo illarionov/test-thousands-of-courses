26e75538f4a1a1581a05897cd6cd76fa2d6e8330
Alexey Illarionov <alexey@0xdc.ru>
2025-08-05 04:07:34 +0300

    Add home screen

--- feature/thcourses-feature-home-impl/src/main/kotlin/presentation/home/HomeFragment.kt
+++ feature/thcourses-feature-home-impl/src/main/kotlin/presentation/home/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.thcourses.feature.home.impl.databinding.FragmentHomeBinding
@@ -25,7 +28,24 @@ internal class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupInsets()
         setupListeners()
+    }
+
+    private fun setupInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.appbar) { v, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                        or WindowInsetsCompat.Type.displayCutout()
+            )
+            v.updatePadding(
+                left = bars.left,
+                top = bars.top,
+                right = bars.right,
+                bottom = 0,
+            )
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     private fun setupListeners() {
